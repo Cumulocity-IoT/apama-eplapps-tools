b5afe52abeea2beb1faceb2ecd2d04d1bd240cd4
Apama Build User (apamabld) <tech-apamabld-ghec@cumulocity.com>
2026-02-02 13:14:17 +0000

    Release 26.275.6

--- testframework/apamax/eplapplications/__init__.py
+++
@@ -1,5 +0,0 @@
-from .platform import CumulocityPlatform
-from .eplapps import EPLApps
-from .connection import C8yConnection
-from .tenant import CumulocityTenant
-from .smartrules import SmartRule, SmartRulesManager
